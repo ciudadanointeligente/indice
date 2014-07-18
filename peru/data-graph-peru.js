6a758c951ab28d783f6a3475dc2a5974b93e9f0b
camargozzini <ca.margozzini@gmail.com>
2014-07-18 17:12:28 -0400

    Peru

--- peru/data-graph-peru.js
+++ peru/data-graph-peru.js
@@ -4,9 +4,9 @@ long_short_data = [
     color: '#f5292e',
     values: [
       { 
-        "label" : "Chile" ,
-        "n_palabras" : 49 ,
-        "value" : 0.49
+        "label" : "Perú" ,
+        "n_palabras" : 59 ,
+        "value" : 0.59
       }
     ]
   },
@@ -15,9 +15,9 @@ long_short_data = [
     color: '#9b3237',
     values: [
       { 
-        "label" : "Chile" ,
-        "n_palabras" : 49 ,
-        "value" : 0.49
+        "label" : "Perú" ,
+        "n_palabras" : 60 ,
+        "value" : 0.60
       }
     ]
   },
@@ -26,9 +26,9 @@ long_short_data = [
     color: '#006fbd',
     values: [
       { 
-        "label" : "Chile" ,
-        "n_palabras" : 50 ,
-        "value" : 0.50
+        "label" : "Perú" ,
+        "n_palabras" : 32 ,
+        "value" : 0.32
       }
     ]
   },
@@ -37,9 +37,9 @@ long_short_data = [
     color: '#00b4f2',
     values: [
       { 
-        "label" : "Chile" ,
-        "n_palabras" : 65 ,
-        "value" : 0.65
+        "label" : "Perú" ,
+        "n_palabras" : 68 ,
+        "value" : 0.68
       },
     ]
   }
@@ -75,7 +75,7 @@ nv.addGraph(function() {
 
   // chart.forceY([0,1]);
 
-  d3.select('#chartChile svg')
+  d3.select('#chartPeru svg')
       .datum(long_short_data)
       .call(chart);
 
